a99a68ede90239b4c892a301c35ad557a74fa148
Davi Silva <davisilvaphoto@gmail.com>
2022-09-05 18:43:47 -0300

    types alias in classes

---
+++ src/classes/implements-types.ts
@@ -0,0 +1,17 @@
+type PersonType = {
+  name: string;
+  lastname: string;
+  fullName: () => string;
+};
+
+export class Person implements PersonType {
+  constructor(public name: string, public lastname: string) {}
+
+  fullName(): string {
+    return `${this.name} ${this.lastname}`;
+  }
+}
+
+const person = new Person("Davi", "Silva");
+
+console.log(person);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { Car } from "./classes/composition";
-
-const car = new Car();
-
-console.log(car.on());
-console.log(car.speedUp());
-console.log(car.stop());
-console.log(car.off());
+console.log("okay");
